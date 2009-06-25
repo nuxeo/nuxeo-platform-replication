afe8b96bf2af799267739e22bfac03ab40a22c03
Bogdan Tatar <btatar@nuxeo.com>
2009-06-25 14:28:47 +0300

    NXP-3755:Import the documentary base Wrong placement of the class

---
+++ nuxeo-platform-replication-common-api/src/main/java/org/nuxeo/ecm/platform/replication/common/ReplicationConstants.java
@@ -0,0 +1,39 @@
+/*
+ * (C) Copyright 2009 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+package org.nuxeo.ecm.platform.replication.common;
+
+/**
+ * Utility class used for registering constants.
+ * 
+ * @author btatar
+ * 
+ */
+public class ReplicationConstants {
+
+    private ReplicationConstants() {
+    }
+
+    public static final String METADATA_FILE_NAME = "metadata.properties";
+
+    public static final String DOCUMENTARY_BASE_LOCATION_NAME = "Documentary Base";
+
+    public static final String USUAL_DOCUMENTS_LOCATION_NAME = "Usual documents";
+
+    public static final String VERSIONS_LOCATION_NAME = "Versions";
+
+    public static final String PROXIES_LOCATION_NAME = "Proxies";
+
+}
