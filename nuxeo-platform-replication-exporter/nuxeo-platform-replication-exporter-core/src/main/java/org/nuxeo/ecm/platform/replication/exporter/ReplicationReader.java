8c66ef8e5d51883ed7de992cf10b736a5516ff47
Radu Darlea <rdarlea@nuxeo.com>
2009-07-03 11:40:56 +0300

    NXP-3641: javadoc for "why not using iteratored query"

--- nuxeo-platform-replication-exporter/nuxeo-platform-replication-exporter-core/src/main/java/org/nuxeo/ecm/platform/replication/exporter/ReplicationReader.java
+++ nuxeo-platform-replication-exporter/nuxeo-platform-replication-exporter-core/src/main/java/org/nuxeo/ecm/platform/replication/exporter/ReplicationReader.java
@@ -1,8 +1,25 @@
+/*
+ * (C) Copyright 2009 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Nuxeo - initial API and implementation
+ */
 package org.nuxeo.ecm.platform.replication.exporter;
 
 import java.io.IOException;
 import java.util.Iterator;
 
+import org.apache.log4j.Logger;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
@@ -11,14 +28,32 @@ import org.nuxeo.ecm.core.io.ExportedDocument;
 import org.nuxeo.ecm.core.io.impl.ExportedDocumentImpl;
 import org.nuxeo.ecm.core.io.impl.plugins.DocumentModelReader;
 
+/**
+ * Reader extension.
+ * 
+ * @author cpriceputu
+ * 
+ */
 public class ReplicationReader extends DocumentModelReader {
+
+    private static final Logger LOG = Logger.getLogger(ReplicationReader.class);
 
     protected Iterator<DocumentModel> iterator = null;
 
+    /**
+     * The documents are retrieved using a session query. The iterated query is
+     * not working see NXP-3814. So the documents are collected in memory. When
+     * the iterated query will be available the memory exhaustion can be
+     * avoided.
+     * 
+     * @param session
+     * @throws ClientException
+     */
     protected ReplicationReader(CoreSession session) throws ClientException {
         super(session);
         DocumentModelList list = session.query("SELECT * FROM Document");
         list.add(session.getRootDocument());
+        LOG.info("Exporting " + list.size() + " documents");
         iterator = list.iterator();
     }
 
