d55514426d6569c6b3931e844abb4db0cd3bab16
Eugen Ionica <eionica@nuxeo.com>
2010-07-30 18:16:38 +0200

    NXP-5050 added unit test for export ecm:childrenOrder

--- nuxeo-platform-replication-exporter/nuxeo-platform-replication-exporter-core/src/test/java/org/nuxeo/ecm/platform/ServiceTest.java
+++ nuxeo-platform-replication-exporter/nuxeo-platform-replication-exporter-core/src/test/java/org/nuxeo/ecm/platform/ServiceTest.java
@@ -16,15 +16,23 @@
 package org.nuxeo.ecm.platform;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.nuxeo.common.utils.StringUtils;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.storage.sql.SQLRepositoryTestCase;
 import org.nuxeo.ecm.platform.replication.common.StatusListener;
 import org.nuxeo.ecm.platform.replication.exporter.DocumentaryBaseExpServiceImpl;
 import org.nuxeo.ecm.platform.replication.exporter.DocumentaryBaseExporterService;
+
 
 public class ServiceTest extends SQLRepositoryTestCase {
     public static final String SCHEMA_BUNDLE = "org.nuxeo.ecm.core.schema";
@@ -103,6 +111,7 @@ public class ServiceTest extends SQLRepositoryTestCase {
         session.save();
     }
 
+    
     public void testService() throws Exception {
         createDataWarehouse();
         DocumentaryBaseExporterService srv = new DocumentaryBaseExpServiceImpl();
@@ -115,6 +124,49 @@ public class ServiceTest extends SQLRepositoryTestCase {
                 "test.folder"), true, true, true);
         //simple avoid closing session before test completes
         Thread.sleep(10000);
+    }
+    
+    
+    public void testDocumentOrder() throws Exception {
+    	 // create the ordered folder
+    	 DocumentModel folder = session.createDocumentModel("/", "0001", "OrderedFolder");
+    	 folder.setPropertyValue("dc:title", "Ordered Folder");
+    	 folder = session.createDocument(folder);
+    	 
+    	 // create files
+    	 List<String> ids =  new ArrayList<String>();
+    	 for ( int i = 0 ; i < 5 ; i++ ){
+    		 String name = "file" + i;
+    		 DocumentModel file = session.createDocumentModel( folder.getPathAsString(), name, "File");
+    	     file.setPropertyValue("dc:title", name);
+    	     file = session.createDocument(file);
+    	     ids.add(file.getId());
+    	 }
+    	 session.save();
+    	 
+    	 // change the creation order to 1 2 0 3 4 
+    	 session.orderBefore(folder.getRef(), "file0", "file3");
+    	 session.save();
+    	 // update the list of ids 
+    	 String id = ids.remove(0);
+    	 ids.add(2, id);
+    	 
+    	 DocumentaryBaseExporterService srv = new DocumentaryBaseExpServiceImpl();
+    	 File exportDirectory = new File ( System.getProperty("java.io.tmpdir"), "testExportOrderedFolder");
+    	 exportDirectory.deleteOnExit();
+    	 
+    	 srv.export("test", null, exportDirectory, true, true, true);
+    	 //simple avoid closing session before test completes
+    	 Thread.sleep(3000);
+    	 
+    	 Properties properties = new Properties();
+    	 File propFile = new File( exportDirectory, "Documentary Base/Usual documents/0001/metadata.properties");
+    	 FileInputStream fis = new FileInputStream(propFile);
+    	 properties.load(fis);
+    	 
+    	 
+    	 String p = properties.getProperty("ecm:childrenOrder");
+    	 assertEquals(StringUtils.join(ids, ";"), p);
     }
 
     public static void main(String[] args) throws Exception {
